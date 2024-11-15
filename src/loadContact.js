c040d833f7d675ad4f4a86d8bfb9abaa6bf310c7
William Tsai <wtsai_us@yahoo.com>
2024-11-14 21:05:13 -0800

    complete menu and contact tabs

--- src/loadContact.js
+++ src/loadContact.js
@@ -4,6 +4,13 @@ const LoadContact = (function() {
         while(content.firstChild) {
             content.removeChild(content.firstChild);
         }
+        const h1 = document.createElement("h1");
+        h1.textContent = "Contact Info";
+        const contact = document.createElement("div");
+        contact.innerHTML = "(123) 456-7890<br>stinkypete99@pmail.com<br>1234 Fake St, Springfield, PA";
+        contact.className = "contact";
+        content.appendChild(h1);
+        content.appendChild(contact);
     }
 
     return { load };
--- src/loadMenu.js
+++ src/loadMenu.js
@@ -1,9 +1,38 @@
+import durianCake from "./img/duriancheesecake.jpg";
+import durianCurry from "./img/duriancurry.jpg";
+import durianPie from "./img/durianpie.jpg";
+
 const LoadMenu = (function() {
     const load = () => {
         const content = document.querySelector("div#content");
         while(content.firstChild) {
             content.removeChild(content.firstChild);
         }
+
+        const h1 = document.createElement("h1");
+        h1.textContent = "Menu";
+        
+        content.appendChild(h1);
+        content.appendChild(loadMenuItem("Durian Cake", "$12.99", durianCake));
+        content.appendChild(loadMenuItem("Durian Curry", "$13.99", durianCurry));
+        content.appendChild(loadMenuItem("Durian Pie", "$14.99", durianPie));
+    }
+
+    const loadMenuItem = (name, price, pic) => {
+        const itemCard = document.createElement("div");
+        itemCard.className = "item";
+
+        const nameE = document.createElement("h2");
+        nameE.textContent = name;
+        const priceE = document.createElement("p");
+        priceE.textContent = price;
+        const picE = document.createElement("img");
+        picE.src = pic;
+        itemCard.appendChild(nameE);
+        itemCard.appendChild(priceE);
+        itemCard.appendChild(picE);
+
+        return itemCard;
     }
 
     return { load };
